246b86fcf6f7853465e556cb6cc895c04ffe8c80
MyGit <toquangsang29012006@gmail.com>
2026-05-16 22:13:30 +0700

    tạo project

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+const route = require("./routes/client/index.route")
+
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+
+// Routes
+route(app);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
