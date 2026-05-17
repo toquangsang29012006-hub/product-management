b5163d85382ebf794fb7837f4616372fcd3bb985
MyGit <toquangsang29012006@gmail.com>
2026-05-17 16:13:08 +0700

    Cấu hình Database

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const mongoose = require("mongoose");
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 
-const route = require("./routes/client/index.route")
+const route = require("./routes/client/index.route");
 
+mongoose.connect(process.env.MONGOOSE_URL);
 
 app.set("views", "./views");
 app.set("view engine", "pug");
