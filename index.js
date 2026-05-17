62f8a939b5763902f57fc3cb98d6b726a8c1ec5a
MyGit <toquangsang29012006@gmail.com>
2026-05-17 15:20:59 +0700

    Nhúng Bootstrap 4 - Nhúng file tĩnh

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
-
-
 const port = process.env.PORT;
 
 const route = require("./routes/client/index.route")
@@ -12,6 +10,7 @@ const route = require("./routes/client/index.route")
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+app.use(express.static('public'));
 
 // Routes
 route(app);
