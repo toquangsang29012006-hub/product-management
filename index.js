ca81e280ed3ad7d893568eea6fe6e2d2bfbc264a
MyGit <toquangsang29012006@gmail.com>
2026-06-23 23:25:02 +0700

    Cấu hình .env

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+require("dotenv").config();
 
 const route = require("./routes/client/index.route");
+
+
+const app = express();
+const port = process.env.PORT;
 
 app.set("views", "./views"); // trỏ về thư mục views ở thư mục gốc của ứng dụng
 app.set("view engine", "pug"); // Template engine mà bạn muốn sử dụng.
