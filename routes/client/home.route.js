730b2254a8b5edb045f44e2f403ea025ccd787c9
MyGit <toquangsang29012006@gmail.com>
2026-06-23 23:16:49 +0700

    Cấu hình folder controllers

--- routes/client/home.route.js
+++ routes/client/home.route.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const route = express.Router();
 
-route.get("/", (req, res) => {
-    res.render("client/pages/home/index");
-});
+const controller = require("../../controllers/client/home.controller");
+
+route.get("/", controller.index);
 
 module.exports = route;
