33d57e5a08fbdfb5d20f25c0c86446a4714a1394
MyGit <toquangsang29012006@gmail.com>
2026-06-24 00:26:04 +0700

    Nhúng Bootstrap 5 và file tĩnh

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const port = process.env.PORT;
 app.set("views", "./views"); // trỏ về thư mục views ở thư mục gốc của ứng dụng
 app.set("view engine", "pug"); // Template engine mà bạn muốn sử dụng.
 
+app.use(express.static('public'));
+
 
 // Route
 route(app);
