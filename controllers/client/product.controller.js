9f4b1ded3e64203efe8c125396b3b075f7117ee4
MyGit <toquangsang29012006@gmail.com>
2026-06-25 10:24:06 +0700

    Cài đặt Mongoose

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,4 +1,4 @@
-module.exports.index = (req, res) => {
+module.exports.index =  (req, res) => {
     res.render("client/pages/products/index", {
         pageTitle: "Trang danh sách sản phẩm"
     });
