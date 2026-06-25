b31c65fd11335e91b7aa8277dfe37b266d4cb20b
MyGit <toquangsang29012006@gmail.com>
2026-06-25 21:59:22 +0700

    Tạo trang tổng quan

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const database = require("./config/database")
 
 const route = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route");
 
 database.connect();
 
@@ -18,6 +19,7 @@ app.use(express.static('public'));
 
 // Route
 route(app);
+routeAdmin(app);
 
 
 app.listen(port, () => {
---
+++ routes/admin/index.route.js
@@ -0,0 +1,7 @@
+const dashboardRoute = require("./dashboard.route");
+const systemConfig = require("../../config/system");
+module.exports = (app) => {
+    const PATH_ADMIN = systemConfig.prefixAdmin;
+
+    app.use(PATH_ADMIN + "/dashboard", dashboardRoute);
+}
