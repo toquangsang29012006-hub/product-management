43359d6ff7684cca823f4dc792fe5e24f1583c89
MyGit <toquangsang29012006@gmail.com>
2026-07-02 00:56:29 +0700

    Tối ưu phần Phân Trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,6 +2,7 @@ const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helper/filterStatus");
 const searchHelper = require("../../helper/search");
+const paginationHelper = require("../../helper/pagination");
 
 // [GET]  /admin/products
 module.exports.index = async (req, res) => {
@@ -19,24 +20,20 @@ module.exports.index = async (req, res) => {
     }
 
     // Pagination
-    let objectPagination = {
-        currentPage: 1,
-        limitItem: 4
-    }
-
-    if(req.query.page){
-        objectPagination.currentPage = parseInt(req.query.page);
-    }
-
-    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItem;
-
     const countProduct = await Product.countDocuments(find);
-    const totalPage = Math.ceil(countProduct/objectPagination.limitItem);
 
-    objectPagination.totalPage = totalPage;
+    let objectPagination = paginationHelper(
+        {
+            currentPage: 1,
+            limitItem: 4
+        },
+        req.query,
+        countProduct
+    )
+
     // End Pagination
 
-    
+
     if(req.query.status){
         find.status = req.query.status;
     }
