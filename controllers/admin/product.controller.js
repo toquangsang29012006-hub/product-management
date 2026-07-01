f846f656c67c6f26e765fa6da1830929757970b5
MyGit <toquangsang29012006@gmail.com>
2026-07-01 22:11:48 +0700

    Tối ưu phần Bộ lọc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,33 +1,11 @@
 const Product = require("../../models/product.model");
 
+const filterStatusHelper = require("../../helper/filterStatus");
+
 // [GET]  /admin/products
 module.exports.index = async (req, res) => {
 
-    const filterStatus = [
-        {
-            name: "Tất cả",
-            status: "",
-            class: ""
-        },
-        {
-            name: "Hoạt động",
-            status: "active",
-            class: ""
-        },
-        {
-            name: "Không hoạt động",
-            status: "inactive",
-            class: ""
-        },
-    ];
-
-    if(req.query.status){
-        const index = filterStatus.findIndex(item => item.status == req.query.status);
-        filterStatus[index].class = "active";
-    } else{
-        const index = filterStatus.findIndex(item => item.status == "");
-        filterStatus[index].class = "active";
-    }
+   const filterStatus = filterStatusHelper(req.query);
 
     const find = {
         deleted: false
