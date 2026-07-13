a3fa822a905bf2b25bda39b27e08c09b5487a8a6
MyGit <toquangsang29012006@gmail.com>
2026-07-13 22:05:13 +0700

    Xóa vĩnh viễn

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -79,3 +79,12 @@ module.exports.changeMulti = async (req, res) => {
     }
     res.redirect(req.get('Referrer') || '/');
 }
+
+// [DELETE] /admin/products/delete/:id
+module.exports.deleteItem = async (req, res) => {
+    const id = req.params.id;
+
+    await Product.deleteOne({_id: id});
+
+    res.redirect(req.get('Referrer') || '/');
+}
--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -19,3 +19,25 @@ if(buttonChangeStatus) {
     });
 }
 // End Change Status
+
+// Delete Item
+const buttonDelete = document.querySelectorAll("[button-delete]");
+if(buttonDelete) {
+    const formDeleteItem = document.querySelector("#form-deleted-item");
+    const path = formDeleteItem.getAttribute("data-path");
+    buttonDelete.forEach(button => {
+        button.addEventListener("click", () => {
+            const isConfirm = confirm("Bạn có chắc muốn xóa sản phẩm này");
+            if(isConfirm){
+                const id = button.getAttribute("data-id");
+                const action = path + `/${id}?_method=DELETE`;
+
+                formDeleteItem.action = action;
+
+                formDeleteItem.submit();
+            }
+        })
+    })
+}
+
+// End Delete Item
