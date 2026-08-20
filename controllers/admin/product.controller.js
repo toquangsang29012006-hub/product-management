3b7c688f34ae23d9dc2f23e47b5efcdcb06284e5
MyGit <toquangsang29012006@gmail.com>
2026-08-20 16:36:15 +0700

    Tính năng xóa nhiều sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -74,6 +74,14 @@ module.exports.changeMulti = async (req, res) => {
         case "inactive":
             await Product.updateMany({_id: {$in: ids} }, {status: "inactive"});
             break;
+        case "delete-all":
+            await Product.updateMany(
+                {_id: {$in: ids}},
+                {
+                    deleted: true,
+                    deleteAt: new Date()
+                }
+            )
         default: 
             break;
     }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -100,6 +100,16 @@ if(formChangeMulti){
             "input[name='id']:checked"
         );
 
+        const typeChange = e.target.elements.type.value;
+
+        if(typeChange == "delete-all"){
+            const isConfirm = confirm("Bạn có chắc muốn xóa sản phẩm này không?");
+
+            if(!isConfirm){
+                return;
+            }
+        }
+
         if(inputsChecked.length > 0){
             let ids = [];
             const inputIds = formChangeMulti.querySelector("input[name='ids']");
